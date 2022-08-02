1c672806f4ff64e7dadbcd21f0eaba6bf3a5c082
Flavio Canedo <flavio_canedo@live.com>
2022-08-02 02:50:57 -0300

    :bug: Fixed namespace on decorator class

--- src/SourceGenerator/SourceGenerators/TestBuilderGenerator.cs
+++ src/SourceGenerator/SourceGenerators/TestBuilderGenerator.cs
@@ -22,34 +22,33 @@ namespace SourceGenerator.SourceGenerators
             foreach (var builderMember in builderMembers)
             {
                 // Finders
-                var typeName = builderMember.MetadataName.Replace("Builder", "");
-                var srcType = typeNamespaceService.GetTypeByClassName(typeName);
-                var properties = srcType.GetMembers().Where(m => m.Kind.Equals(SymbolKind.Property));
-
+                var className = builderMember.MetadataName.Replace("Builder", "");
+                var classSymbol = typeNamespaceService.GetTypeByClassName(className);
+                var classProperties = classSymbol.GetMembers().Where(m => m.Kind.Equals(SymbolKind.Property));
 
                 // Customizations
                 var codeGen = new StringBuilder();
-
+                
                 codeGen
                     .AppendLine("// Generated at " + DateTime.Now)
                     .AppendLine("")
-                    .AppendLine("using SourceGeneratorTest.Models;")
+                    .AppendLine($"using {classSymbol.ContainingNamespace};")
                     .AppendLine("using System.Runtime.InteropServices;")
                     .AppendLine("")
                     .AppendLine("namespace SourceGeneratorTest.Builders")
                     .AppendLine("{")
-                    .AppendLine(Tab(1) + FullClassName(typeName))
+                    .AppendLine(Tab(1) + FullClassName(className))
                     .AppendLine(Tab(1) + "{")
-                    .AppendLine(Tab(2) + $"public static {typeName} CreateDefault([Optional] {typeName} src)")
+                    .AppendLine(Tab(2) + $"public static {className} CreateDefault([Optional] {className} src)")
                     .AppendLine(Tab(2) + "{")
-                    .AppendLine(Tab(3) + $"return src ?? new {typeName}();")
+                    .AppendLine(Tab(3) + $"return src ?? new {className}();")
                     .AppendLine(Tab(2) + "}");
 
-                foreach (IPropertySymbol property in properties)
+                foreach (IPropertySymbol property in classProperties)
                 {
                     codeGen
                         .AppendLine("")
-                        .AppendLine(Tab(2) + $"public static {typeName} With{property.Name}(this {typeName} src, {property.Type.Name} value)")
+                        .AppendLine(Tab(2) + $"public static {className} With{property.Name}(this {className} src, {property.Type.Name} value)")
                         .AppendLine(Tab(2) + "{")
                         .AppendLine(Tab(3) + $"src.{property.Name} = value;")
                         .AppendLine(Tab(3) + "return src;")
@@ -61,7 +60,7 @@ namespace SourceGenerator.SourceGenerators
                     .AppendLine("}");
 
                 // Creation
-                context.AddSource(ClassName(typeName), SourceText.From(codeGen.ToString(), Encoding.UTF8));
+                context.AddSource(ClassName(className), SourceText.From(codeGen.ToString(), Encoding.UTF8));
             }
         }
 
